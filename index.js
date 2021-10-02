6e498fc41bd57f64f6b4be80e6813ef65138c3b2
ThangPham <thangpham.it92@gmail.com>
2021-10-02 10:58:56 +0700

    make it better

--- index.js
+++ index.js
@@ -35,29 +35,29 @@ app.get('/', (req, res) => {
 // middleware router
 app.use('/auth', authRoute);
 
-app.post('/', (req, res) => {
-    const { message, user: sender, type, members } = req.body;
+// app.post('/', (req, res) => {
+//     const { message, user: sender, type, members } = req.body;
 
-    if (type === 'message.new') {
-        members
-            .filter((member) => member.user_id !== sender.id)
-            .forEach(({ user }) => {
-                if (!user.online) {
-                    twilioClient.messages.create({
-                        body: `You have a new message from ${message.user.fullName} - ${message.text}`,
-                        messagingServiceSid: messagingServiceSid,
-                        to: user.phoneNumber
-                    })
-                    .then(() => console.log('Message sent!'))
-                    .catch((err) => console.log(err));
-                }
-            })
+//     if (type === 'message.new') {
+//         members
+//             .filter((member) => member.user_id !== sender.id)
+//             .forEach(({ user }) => {
+//                 if (!user.online) {
+//                     twilioClient.messages.create({
+//                         body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+//                         messagingServiceSid: messagingServiceSid,
+//                         to: user.phoneNumber
+//                     })
+//                     .then(() => console.log('Message sent!'))
+//                     .catch((err) => console.log(err));
+//                 }
+//             })
 
-        return res.status(200).send('Message sent!');
-    }
+//         return res.status(200).send('Message sent!');
+//     }
 
-    return res.status(200).send('Not a new message request');
-});
+//     return res.status(200).send('Not a new message request');
+// });
 
 app.listen(port, err => {
     if (err) {
